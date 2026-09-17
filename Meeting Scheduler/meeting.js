9223446a0a384869d3f51a42a2ca6a1d70c31a15
Qazzal-Bisdt <600757@STudent.belgiumcampus.ac.za>
2026-09-17 15:07:57 +0200

    Implement meeting link creation and display
    Added functionality to create a meeting link and display it.

--- Meeting Scheduler/meeting.js
+++ Meeting Scheduler/meeting.js
@@ -1,1 +1,21 @@
+const form = document.getElementById('meetingform');
+const linkcreated = document.getElementById('linkcreated');
+const linkpost = document.getElementById('linkpost');
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); //stop the page from reloading.
 
+    const title = document.getElementById('meeting name'); //if something goes wrong
+    const datetime = document.getElementById('datetime-local');
+    
+    const link = `${window.location.origin}${window.location.pathname}?title=${encodeURIComponent(title)}&time=${utcTimestamp}`;
+    linkcreated.innerHTML = '<a href="${link}">${link}</a>'
+});
+
+const params = new URLSearchParams(window.location.search);
+if(params.has('time')){
+    const title = params.get('title');
+    const utcTimestamp = parseInt(params.get('time'));
+    const localdate = new Date(utcTimestamp);
+
+    linkpost = `${title} at ${localdate.toString()}`;
+}; 
